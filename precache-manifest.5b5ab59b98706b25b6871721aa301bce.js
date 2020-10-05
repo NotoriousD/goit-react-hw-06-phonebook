b11e3d47c0f83cfb930b043b13ebd8c105175a51
Danil Kruhlikov <danylo.kruhlikov@nure.ua>
2020-10-05 16:12:58 +0300

    6 hw deploy

---
+++ precache-manifest.5b5ab59b98706b25b6871721aa301bce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4209b2f8f6b6a423594376eb0bcb1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a9c72f8118915649a18",
+    "url": "/static/css/main.1e77fae1.chunk.css"
+  },
+  {
+    "revision": "c57ee4971c155d80fd5f",
+    "url": "/static/js/2.583405f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.583405f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9c72f8118915649a18",
+    "url": "/static/js/main.b6b1b8a9.chunk.js"
+  },
+  {
+    "revision": "509037c70ced1433267c",
+    "url": "/static/js/runtime-main.36888773.js"
+  }
+]);
--- src/Redux/Actions/filter.js
+++
@@ -1,6 +0,0 @@
-const filterByNames = (value) => ({
-  type: "FILTER_BY_NAME",
-  payload: value,
-});
-
-export default filterByNames;
